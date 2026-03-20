3abf85dd6352a45f1d7efc432b339486764a7730
Verty Kenway <pratamapasya1975@gmail.com>
2026-03-20 22:13:27 +0700

    script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  'Mbel',//untuk deskripsi planet layer 1
+  "Maafin aku",//untuk deskripsi planet layer 2
+  "Udahan Yaa Betenya",//untuk deskripsi planet layer 3
+  "Paris sayang banget sama belva, i love you",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
